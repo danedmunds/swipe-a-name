3bf870ec78c3d825392cecf8ecbcb0a7118bd4db
Dan Edmunds <dan.edmunds@qlik.com>
2018-02-24 16:25:20 -0500

    allow filtering by sex in client

--- client/components/name/name.ctl.js
+++ client/components/name/name.ctl.js
@@ -12,24 +12,25 @@
       return $state.go('login')
     }
 
-    $scope.toggleLeft = buildToggler('left');
-
-    function buildToggler(componentId) {
-      return function() {
-        $mdSidenav(componentId).toggle();
-      };
-    }
-
     var names = [];
 
-    getNamesBatch().then(function success (res) {
-      $timeout(function () {
-        names = res.data.data
-        $scope.current = getNext()
-      }, 0)
-    }).catch(function error (err) {
-      console.log(err)
+    $mdSidenav('left', true).then(function(leftPanel) {
+      var preferences
+      $scope.toggleLeft = function () {
+        leftPanel.toggle()
+        if (leftPanel.isOpen()) {
+          preferences = _.cloneDeep($scope.preferences)
+        }
+      }
+      leftPanel.onClose(function () {
+        if (!_.isEqual(preferences, $scope.preferences)) {
+          getNewNamesBatch()
+        }
+      });
     });
+
+    loadPreferences();
+    getNewNamesBatch();
 
     $scope.toss = function() {
       sendRating($scope.current, 'toss');
@@ -41,8 +42,24 @@
       $scope.current = getNext()
     };
 
+    $scope.savePreferences = savePreferences;
+
+    function getNewNamesBatch () {
+      $scope.current = undefined;
+      names = [];
+      getNamesBatch().then(function success (res) {
+        $timeout(function () {
+          names = res.data.data
+          $scope.current = getNext()
+        }, 0)
+      }).catch(function error (err) {
+        console.log(err)
+      });
+    }
+
     function getNamesBatch() {
-      return $http.get('/api/v1/names?rated=false&sample=true&limit=50');
+      var sex = $scope.preferences.sex ? `&sex=${$scope.preferences.sex}` : ''
+      return $http.get(`/api/v1/names?rated=false&sample=true&limit=50${sex}`);
     }
 
     function getNext() {
@@ -66,6 +83,14 @@
       }).catch(function error () {
 
       })
+    }
+
+    function loadPreferences () {
+      $scope.preferences = JSON.parse(localStorage.getItem('preferences') || '{}');
+    }
+
+    function savePreferences () {
+      localStorage.setItem('preferences', JSON.stringify($scope.preferences));
     }
   }
 })();
